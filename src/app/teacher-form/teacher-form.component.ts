be61cc36784bd303fff9626f0a2c3d9604fc7aed
carolin randala <carolinrandala@gmail.com>
2023-02-13 19:32:55 +0200

    Finished angular forms task

---
+++ src/app/teacher-form/teacher-form.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {Student} from "../shared/models/Student";
+import {Teacher} from "../shared/models/Teacher";
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'app-teacher-form',
+  templateUrl: './teacher-form.component.html',
+  styleUrls: ['./teacher-form.component.css']
+})
+export class TeacherFormComponent {
+
+  teachers: Teacher[] = [];
+
+  tFirstName: string = '';
+  tLastName: string = '';
+
+  tAge: number = 38;
+
+  tSubject: string = '';
+
+
+
+  addTeacher(teacherForm: NgForm) {
+    this.teachers.push(new Teacher(this.tFirstName, this.tLastName, this.tAge, this.tSubject));
+    teacherForm.reset();
+  }
+}
